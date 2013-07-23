e283120c5034fa5f1e5bbe1e7c393167af40b971
moandji.ezana <mwanji@gmail.com>
2013-07-23 14:35:17 +0200

    Added shell scripts.

---
+++ src/main/java/org/webjars/WebJarsCommandLine.java
@@ -0,0 +1,27 @@
+package org.webjars;
+
+import org.apache.maven.plugin.logging.SystemStreamLog;
+
+public class WebJarsCommandLine {
+
+  public static void main(String[] args) {
+    SystemStreamLog log = new SystemStreamLog();
+    String command = args[0];
+    WebJars webJars = new WebJars(log);
+
+    if ("list".equals(command)) {
+      webJars.list(args.length > 1 ? args[1] : null);
+    } else if ("install".equals(command)) {
+      if (args.length < 2) {
+        log.error("Please specify a WebJar to be installed");
+        return;
+      }
+      webJars.install(args[1]);
+    } else if ("help".equals(command)) {
+      webJars.help();
+    } else {
+      log.error("Invalid command.");
+      webJars.help();
+    }
+  }
+}
