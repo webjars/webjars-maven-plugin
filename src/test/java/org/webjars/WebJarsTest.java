7c77f96c9f9707e4448df90735ebc276a638bf38
moandji.ezana <mwanji@gmail.com>
2014-10-10 10:44:14 +0200

    Increased number of results to 5000 to get all webjars

--- src/test/java/org/webjars/WebJarsTest.java
+++ src/test/java/org/webjars/WebJarsTest.java
@@ -1,50 +1,52 @@
 package org.webjars;
 
 import static org.hamcrest.core.StringStartsWith.startsWith;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
 import java.util.Iterator;
 
-import org.apache.maven.artifact.versioning.ArtifactVersion;
 import org.apache.maven.plugin.logging.SystemStreamLog;
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.google.common.base.Splitter;
-import com.google.common.collect.Multimap;
+import com.google.common.collect.Ordering;
 
+/**
+ * NOTE: Tests check the logs rather than the object model because the logs are what matter, as they are what the user sees.
+ */
 public class WebJarsTest {
 
   private final StringBuilder log = new StringBuilder();
 
   @Test
   public void should_put_exact_match_first() throws Exception {
-    Multimap<String, ArtifactVersion> artifacts = new WebJars(getLogger()).list("bootstrap");
+    new WebJars(getLogger()).list("bootstrap");
 
     Iterator<String> logIterator = getLogs();
-    String logL = logIterator.next();
-    Iterator<String> artifactsIterator = artifacts.keySet().iterator();
-    artifactsIterator.next();
-    String secondArtifact = artifactsIterator.next();
+    String logs = logIterator.next();
 
-    assertEquals("bootstrap", secondArtifact);
-    assertThat(logL, startsWith("bootstrap ["));
+    assertThat(logs, startsWith("bootstrap ["));
   }
 
   @Test
   public void should_sort_alphabetically_if_no_exact_match() throws Exception {
-    Multimap<String, ArtifactVersion> artifacts = new WebJars(getLogger()).list("bootstra");
+    new WebJars(getLogger()).list("bootstra");
 
     Iterator<String> logIterator = getLogs();
-    String logL = logIterator.next();
-    Iterator<String> artifactsIterator = artifacts.keySet().iterator();
-    String firstArtifact = artifactsIterator.next();
-    String secondArtifact = artifactsIterator.next();
-
-    assertEquals("angular-ui-bootstrap", firstArtifact);
-    assertEquals("bootstrap", secondArtifact);
-    assertThat(logL, startsWith("angular-ui-bootstrap ["));
+    logIterator.next();
+    
+    String previousLine = null;
+    while (logIterator.hasNext()) {
+      String line = logIterator.next();
+      if (previousLine == null) {
+        previousLine = line;
+        continue;
+      }
+      
+      Assert.assertTrue(Ordering.natural().compare(previousLine, line) < 0);
+    }
   }
 
   @Test
@@ -57,7 +59,7 @@ public class WebJarsTest {
   }
 
   private Iterator<String> getLogs() {
-    Iterator<String> logIterator = Splitter.on('\n').split(log).iterator();
+    Iterator<String> logIterator = Splitter.on('\n').omitEmptyStrings().split(log).iterator();
     logIterator.next();
     return logIterator;
   }
@@ -67,7 +69,7 @@ public class WebJarsTest {
       @Override
       public void info(CharSequence content) {
         log.append(content);
-        super.info(content);
+//        super.info(content);
       }
     };
   }
